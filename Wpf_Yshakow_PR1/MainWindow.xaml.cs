75491080c8628f34884d2588c1ada9c516c833f9
mambabusin142@mail.ru <mambabusin142@mail.ru>
2022-09-23 15:55:51 +1000

    Выполненые все примеры

--- Wpf_Yshakow_PR1/MainWindow.xaml.cs
+++ Wpf_Yshakow_PR1/MainWindow.xaml.cs
@@ -36,5 +36,40 @@ namespace Wpf_Yshakow_PR1
             window1.Show();
         }
 
+        private void Win2_Click(object sender, RoutedEventArgs e)
+        {
+            Window2 window2 = new Window2();
+            window2.Show();
+        }
+
+        private void Win3_Click(object sender, RoutedEventArgs e)
+        {
+            Window3 window3 = new Window3();
+            window3.Show();
+        }
+
+        private void Win4_Click(object sender, RoutedEventArgs e)
+        {
+            Window4 window4 = new Window4();
+            window4.Show();
+        }
+
+        private void Win5_Click(object sender, RoutedEventArgs e)
+        {
+            Window5 window5 = new Window5();
+            window5.Show();
+        }
+
+        private void Win6_Click(object sender, RoutedEventArgs e)
+        {
+            Window6 window6 = new Window6();
+            window6.Show();
+        }
+
+        private void Win7_Click(object sender, RoutedEventArgs e)
+        {
+            Window7 window7 = new Window7();
+            window7.Show();
+        }
     }
 }
